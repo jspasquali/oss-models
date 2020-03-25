1e1675a12ea6cbf1f8a5a7a1e3d6c6bbbf376463
bruceskingle <bruce.skingle@symphony.com>
2020-03-25 19:03:20 +0000

    ServiceInfo

---
+++ core-model/src/main/java/com/symphony/oss/models/core/Application.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Symphony Communication Services, LLC.
+ *
+ * All Rights Reserved
+ */
+
+package com.symphony.oss.models.core;
+
+import org.symphonyoss.s2.fugue.IFugueApplication;
+
+public enum Application implements IFugueApplication
+{
+  Init(7901)
+  ;
+
+  private final int httpPort_;
+  
+  private Application(int httpPort)
+  {
+    httpPort_ = httpPort;
+  }
+
+  @Override
+  public String getName()
+  {
+    return toString();
+  }
+
+  @Override
+  public int getHttpPort()
+  {
+    return httpPort_;
+  }
+
+}
