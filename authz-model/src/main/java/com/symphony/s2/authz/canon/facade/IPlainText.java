63d0b7bdea8d1fa71782d23225611e68b7fa107b
GeremiaLongobardo <67149633+GeremiaLongobardo@users.noreply.github.com>
2020-09-29 15:20:35 +0200

    2020 09 24 pmp admin ui (#23)
    * plaintext for pmp
    
    * added Billing Service

---
+++ authz-model/src/main/java/com/symphony/s2/authz/canon/facade/IPlainText.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2020 Symphony Communication Services, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *----------------------------------------------------------------------------------------------------
+ * Proforma generated from
+ *		Template groupId		 org.symphonyoss.s2.canon
+ *           artifactId canon-template-java
+ *		Template name		   proforma/java/Object/I_.java.ftl
+ *		Template version	   1.0
+ *  At                  2020-09-23 18:44:28 CEST
+ *----------------------------------------------------------------------------------------------------
+ */
+
+package com.symphony.s2.authz.canon.facade;
+
+import javax.annotation.concurrent.Immutable;
+
+import com.symphony.s2.authz.canon.IPlainTextEntity;
+
+/**
+ * Facade for Object ObjectSchema(PlainText)
+ *
+ * A text
+ * Generated from ObjectSchema(PlainText) at #/components/schemas/PlainText
+ */
+@Immutable
+public interface IPlainText
+  extends IPlainTextEntity
+{
+}
+/*----------------------------------------------------------------------------------------------------
+ * End of template proforma/java/Object/I_.java.ftl
+ * End of code generation
+ *------------------------------------------------------------------------------------------------- */
---
+++ authz-model/src/main/java/com/symphony/s2/authz/canon/facade/PlainText.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2020 Symphony Communication Services, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *----------------------------------------------------------------------------------------------------
+ * Proforma generated from
+ *		Template groupId		 org.symphonyoss.s2.canon
+ *           artifactId canon-template-java
+ *		Template name		   proforma/java/Object/_.java.ftl
+ *		Template version	   1.0
+ *  At                  2020-09-23 18:44:28 CEST
+ *----------------------------------------------------------------------------------------------------
+ */
+
+package com.symphony.s2.authz.canon.facade;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.symphony.oss.commons.immutable.ImmutableByteArray;
+
+import com.symphony.oss.commons.dom.json.ImmutableJsonObject;
+import com.symphony.oss.commons.dom.json.MutableJsonObject;
+
+import com.symphony.oss.canon.runtime.IEntity;
+import com.symphony.oss.canon.runtime.IModelRegistry;
+
+
+import com.symphony.s2.authz.canon.PlainTextEntity;
+import com.symphony.s2.authz.canon.IPlainTextEntity;
+import com.symphony.s2.authz.canon.AuthzModel;
+
+/**
+ * Facade for Object ObjectSchema(PlainText)
+ *
+ * A text
+ * Generated from ObjectSchema(PlainText) at #/components/schemas/PlainText
+ */
+@Immutable
+@SuppressWarnings("unused")
+public class PlainText extends PlainTextEntity implements IPlainText
+{
+  /**
+   * Constructor from builder.
+   * 
+   * @param builder A mutable builder containing all values.
+   */
+  public PlainText(AbstractPlainTextBuilder<?,?> builder)
+  {
+    super(builder);
+  }
+  
+  /**
+   * Constructor from serialised form.
+   * 
+   * @param jsonObject An immutable JSON object containing the serialized form of the object.
+   * @param modelRegistry A model registry to use to deserialize any nested objects.
+   */
+  public PlainText(ImmutableJsonObject jsonObject, IModelRegistry modelRegistry)
+  {
+    super(jsonObject, modelRegistry);
+  }
+  
+  /**
+   * Constructor from mutable JSON object.
+   * 
+   * @param mutableJsonObject A mutable JSON object containing the serialized form of the object.
+   * @param modelRegistry A model registry to use to deserialize any nested objects.
+   */
+  public PlainText(MutableJsonObject mutableJsonObject, IModelRegistry modelRegistry)
+  {
+    super(mutableJsonObject, modelRegistry);
+  }
+   
+  /**
+   * Copy constructor.
+   * 
+   * @param other Another instance from which all attributes are to be copied.
+   */
+  public PlainText(IPlainText other)
+  {
+    super(other);
+  }
+  
+  @Override
+  public ImmutableByteArray serialize()
+  {
+    return ImmutableByteArray.newInstance(getText());
+  }
+  
+}
+/*----------------------------------------------------------------------------------------------------
+ * End of template proforma/java/Object/_.java.ftl
+ * End of code generation
+ *------------------------------------------------------------------------------------------------- */
